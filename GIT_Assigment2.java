d1c5f0972354bc91111b889052e8084f283b3e96
Bora Yetkin <borayetkin@gmail.com>
2023-02-12 18:30:28 +0300

    average methods added
    getAverage and displayDifferenceFromAverage methods are added.

---
+++ GIT_Assigment2.java
@@ -0,0 +1,24 @@
+public class GIT_Assigment2 {
+
+    public int getAverage(int[] arr){
+        int sum = 0;
+        int average = 0;
+
+        for(int i=0; i<arr.length; i++){
+            sum = sum + arr[i];
+            average = sum / arr.length;
+        } 
+        return average;
+    }
+
+    public void displayDifferenceFromAverage(int[] arr){
+        int average = getAverage(arr);
+        int[] arr1 = new int[arr.length];
+
+        for(int i=0; i<arr.length; i++){
+            arr1[i] = arr[i] - average;
+            System.out.print(arr1[i] + " ");
+        }
+
+    }   
+}
